6651eae5af4a342b0595a14963999a2579781296
Felipe Salazar <felipesalazarcruz4@gmail.com>
2024-03-19 14:59:31 -0500

    Ajuste JS y prueba responsive (nw)

--- index.js
+++ index.js
@@ -11,15 +11,38 @@ function encriptar() {
         .replace(/o/gi, "ober")
         .replace(/u/gi, "ufat");
         
-if(text.value != 0){
+    if(texto.value != 0) {
     document.getElementById("ingreseTexto").value = textoCifrado;
     tituloMensaje.textContent = "Texto encriptado con éxito";
     parrafo.textContent = "";
     dibujo.src = "./images/Encriptado.png";
-} else {
+    } else {
     dibujo.src = "./images/Muñeco.png";
     tituloMensaje.textContent = "Ningún mensaje fue encotrado";
     parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar.";
     alert("Debes ingresar algún texto");
+    }
 }
+
+function desencriptar() {
+    let texto = document.getElementById("ingreseTexto").value;
+
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi, "i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+    if(texto.value != 0){
+    document.getElementById("ingreseTexto").value = textoCifrado;
+    tituloMensaje.textContent = "Texto desencriptado con éxito";
+    parrafo.textContent = "";
+    dibujo.src = "./images/Desencriptado.png";
+    } else {
+    dibujo.src = "./images/Muñeco.png";
+    tituloMensaje.textContent = "Ningún mensaje fue encotrado";
+    parrafo.textContent = "Ingresa el texto que desees encriptar o desencriptar.";
+    alert("Debes ingresar algún texto");
+    }
 }
